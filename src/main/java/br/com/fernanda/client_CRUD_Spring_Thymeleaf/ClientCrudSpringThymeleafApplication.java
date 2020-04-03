612814cdd022c7abc78f08908daeb36b34227bd0
fernanda.g.belchior <fernanda.g.belchior@accenture.com>
2020-04-03 10:20:37 -0300

    Commit 01 - project creation and configuration by Spring Initializr

---
+++ src/main/java/br/com/fernanda/client_CRUD_Spring_Thymeleaf/ClientCrudSpringThymeleafApplication.java
@@ -0,0 +1,13 @@
+package br.com.fernanda.client_CRUD_Spring_Thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientCrudSpringThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientCrudSpringThymeleafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fernanda/client_CRUD_Spring_Thymeleaf/ClientCrudSpringThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fernanda.client_CRUD_Spring_Thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientCrudSpringThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
